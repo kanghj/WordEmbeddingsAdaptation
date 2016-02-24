9355acdf31d4aef0776c661553bf5e32e18cda49
Kang HJ <kanghongjin@gmail.com>
2016-02-24 15:28:00 +0800

    scale embeddings script

---
+++ scale-embeddings.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+"""
+found on http://metaoptimize.com/projects/wordreprs/ and adapted
+
+"""
+
+words = []
+values = []
+import sys,string, numpy
+for l in sys.stdin:
+	d = string.split(l)
+	#words.append(d[0])
+	values.append([float(x) for x in d[1:]])
+
+values = numpy.array(values)
+values /= numpy.std(values)
+values *= 0.1
+
+for i in range(len(values)):
+	#print words[i],
+	for v in values[i]:
+		print v,
+	print
